99f56f2e7953ae890ba7684f854bff5d14f0f921
Anna G <anna.grosman.qa@gmail.com>
2024-03-21 10:58:31 -0500

    CAP-441-Task-4

--- features/environment.py
+++ features/environment.py
@@ -14,19 +14,19 @@ from app.application import Application
 #  behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/main_page_ui.feature
 
 
-def browser_init(context):
-    """
-    Initialize the web driver for the tests.
-    :param context: Behave context
-    """
+# def browser_init(context):
+#     """
+#     Initialize the web driver for the tests.
+#     :param context: Behave context
+#     """
 
 ### BROWSERSTACK ###
-# def browser_init(context, scenario_name):
-#     """
-#         Initialize the web driver for the tests.
-#         :param context: Behave context
-#         :param scenario_name: Name of the scenario
-#         """
+def browser_init(context, scenario_name):
+    """
+        Initialize the web driver for the tests.
+        :param context: Behave context
+        :param scenario_name: Name of the scenario
+        """
 
     # driver_path = ChromeDriverManager().install()
     # service = Service(driver_path)
@@ -44,34 +44,34 @@ def browser_init(context):
     # context.driver = webdriver.Safari()
 
     ### HEADLESS MODE ####
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    service = Service(ChromeDriverManager().install())
-    context.driver = webdriver.Chrome(
-        options=options,
-        service=service
-    )
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # service = Service(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
 
     ### BROWSERSTACK ###
-    # # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    # bs_user = ''
-    # bs_key = ''
-    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    #
-    # options = Options()
-    # bstack_options = {
-    #     'os': 'Windows',
-    #     'osVersion': '10',
-    #     'browserName': 'Edge',
-    #     'sessionName': scenario_name
-    # }
-    # options.set_capability('bstack:options', bstack_options)
-    # context.driver = webdriver.Remote(command_executor=url, options=options)
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user = 'annagrosman_jBqX1R'
+    bs_key = '4sMpWHpU7jNGqr1yU6yj'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
 
-    # context.driver.maximize_window()
+    options = Options()
+    bstack_options = {
+        'os': 'Windows',
+        'osVersion': '10',
+        'browserName': 'Edge',
+        'sessionName': scenario_name
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
+    context.driver.maximize_window()
 
     ### HEADLESS MODE ####
-    context.driver.set_window_size(1024, 768)
+    # context.driver.set_window_size(1024, 768)
 
     context.driver.implicitly_wait(10)
     context.wait = WebDriverWait(context.driver, 15)
@@ -85,9 +85,9 @@ def before_scenario(context, scenario):
 
 
 ### BROWSERSTACK ###
-# def before_scenario(context, scenario):
-#     print('\nStarted scenario: ', scenario.name)
-#     browser_init(context, scenario.name)
+def before_scenario(context, scenario):
+    print('\nStarted scenario: ', scenario.name)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
