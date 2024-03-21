08ef22172981675991a70d212b6df192a97ed10a
Anna G <anna.grosman.qa@gmail.com>
2024-03-21 10:45:08 -0500

    CAP-441-Task-4

--- features/environment.py
+++ features/environment.py
@@ -14,19 +14,19 @@ from app.application import Application
 #  behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/main_page_ui.feature
 
 
-# def browser_init(context):
-#     """
-#     Initialize the web driver for the tests.
-#     :param context: Behave context
-#     """
+def browser_init(context):
+    """
+    Initialize the web driver for the tests.
+    :param context: Behave context
+    """
 
 ### BROWSERSTACK ###
-def browser_init(context, scenario_name):
-    """
-        Initialize the web driver for the tests.
-        :param context: Behave context
-        :param scenario_name: Name of the scenario
-        """
+# def browser_init(context, scenario_name):
+#     """
+#         Initialize the web driver for the tests.
+#         :param context: Behave context
+#         :param scenario_name: Name of the scenario
+#         """
 
     # driver_path = ChromeDriverManager().install()
     # service = Service(driver_path)
@@ -43,48 +43,51 @@ def browser_init(context, scenario_name):
     ### SAFARI ###
     # context.driver = webdriver.Safari()
 
-    ## HEADLESS MODE ####
-    # options = webdriver.ChromeOptions()
-    # options.add_argument('headless')
-    # service = Service(ChromeDriverManager().install())
-    # context.driver = webdriver.Chrome(
-    #     options=options,
-    #     service=service
-    # )
+    ### HEADLESS MODE ####
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    service = Service(ChromeDriverManager().install())
+    context.driver = webdriver.Chrome(
+        options=options,
+        service=service
+    )
 
     ### BROWSERSTACK ###
-    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    bs_user = ''
-    bs_key = ''
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    # # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    # bs_user = ''
+    # bs_key = ''
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     'os': 'Windows',
+    #     'osVersion': '10',
+    #     'browserName': 'Edge',
+    #     'sessionName': scenario_name
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
 
-    options = Options()
-    bstack_options = {
-        'os': 'Windows',
-        'osVersion': '10',
-        'browserName': 'Edge',
-        'sessionName': scenario_name
-    }
-    options.set_capability('bstack:options', bstack_options)
-    context.driver = webdriver.Remote(command_executor=url, options=options)
+    # context.driver.maximize_window()
 
-    context.driver.maximize_window()
+    ### HEADLESS MODE ####
+    context.driver.set_window_size(1024, 768)
 
     context.driver.implicitly_wait(10)
     context.wait = WebDriverWait(context.driver, 15)
     context.app = Application(context.driver)
 
 
-# def before_scenario(context, scenario):
-#     # print('\nStarted scenario: ', scenario.name) # print or logger
-#     logger.info(f"Started scenario: {scenario.name}")
-#     browser_init(context)
+def before_scenario(context, scenario):
+    # print('\nStarted scenario: ', scenario.name) # print or logger
+    logger.info(f"Started scenario: {scenario.name}")
+    browser_init(context)
 
 
 ### BROWSERSTACK ###
-def before_scenario(context, scenario):
-    print('\nStarted scenario: ', scenario.name)
-    browser_init(context, scenario.name)
+# def before_scenario(context, scenario):
+#     print('\nStarted scenario: ', scenario.name)
+#     browser_init(context, scenario.name)
 
 
 def before_step(context, step):
--- pages/menu_block.py
+++ pages/menu_block.py
@@ -1,16 +1,11 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from time import sleep
 
 from pages.base_page import Page
 
 
 class MenuBlock(Page):
-
-    # SETTINGS_BTN = (By.CSS_SELECTOR, ".menu_block_1 [href*='settings']")
-    SETTINGS_BTN = (By.XPATH, "//a[contains(@href, '/settings') and contains(., 'Settings')]")
+    SETTINGS_BTN = (By.CSS_SELECTOR, ".menu_block_1 [href*='settings']")
 
     def click_settings(self):
-        sleep(50)
-        self.wait.until(EC.visibility_of_element_located(self.SETTINGS_BTN), f"Element is not visible")
         self.wait_element_clickable_click(*self.SETTINGS_BTN)
